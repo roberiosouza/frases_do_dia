67940e32384342eb231f46ec46c9ee604ba83811
Robério <rroberiosouza@gmail.com>
2024-05-13 21:43:30 -0300

    Adicionado um botão

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Frases do Dia",
     home: Container(
       color: Colors.white,
@@ -15,7 +16,19 @@ void main(){
               fontWeight: FontWeight.normal,
               decoration: TextDecoration.none
             ),
-          )
+          ),
+          TextButton(
+              onPressed: (){
+                print("Botão pressionado.");
+              },
+              child: Text(
+                  "Clique aqui",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                    decoration: TextDecoration.none,
+                  ),
+              ))
         ],
       ),
     )
